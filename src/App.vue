8738ffe79be075110d49a221de92536cd1f77f59
Tim Jack Schmitt <timjack.home.mail@gmail.com>
2023-12-29 21:24:03 +0100

    added view to the app and changed some colors

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
+    <router-link to="/game">Game</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view />
 </template>
 
-<style>
+<style lang="scss">
+body {
+  background-color: $outer-space;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: $font-primary, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $off-white-dark;
 }
-
+h1 {
+  color: $rose;
+}
 nav {
   padding: 30px;
 }
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: $light-coral;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: $teal-green;
 }
 </style>
