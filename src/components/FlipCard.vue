b1454af9010c981e5f5b4c81a64e6c08698c30a1
Tim Jack Schmitt <timjack.home.mail@gmail.com>
2024-01-18 20:41:38 +0100

    created charater flip cards

---
+++ src/components/FlipCard.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="flip-card">
+    <div class="flip-card-inner">
+      <div class="flip-card-front">
+        <img :src="image" alt="Card Image" />
+      </div>
+      <div class="flip-card-back">
+        <h2>{{ title }}</h2>
+        <p>{{ description }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: String,
+    title: String,
+    description: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+//image ratios
+$imgWidth: 1024px;
+$imgHeight: 1792px;
+.flip-card {
+  width: calc($imgWidth * 0.33); /* Adjust as needed */
+  height: calc($imgHeight * 0.33); /* Adjust for 1:2 ratio */
+  perspective: 1000px; /* Depth of the flip effect */
+}
+
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+
+.flip-card:hover .flip-card-inner {
+  transform: rotateY(180deg); /* Flip effect */
+}
+
+.flip-card-front,
+.flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+
+.flip-card-front {
+}
+
+.flip-card-back {
+  transform: rotateY(180deg); /* Align backside correctly */
+  background-color: $black-82;
+  color: $off-white;
+
+  backdrop-filter: brightness(100%) blur(3px);
+
+  backface-visibility: hidden; // Apply to both front and back
+  -webkit-backface-visibility: hidden; // For Safari and older browsers
+}
+
+.flip-card-back h2,
+.flip-card-back p {
+  margin: 10px; /* Spacing for text */
+}
+
+img {
+  width: 100%;
+}
+</style>
